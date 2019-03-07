2673c7d19fc6af3e1bef06e7301649d9971da95c
SuperWeebTanks <trilleo12@live.dk>
2019-03-07 13:11:20 +0100

    Added Projects

---
+++ Exercise_6_c# (CLIENT)/Exercise_6_c#/file_client/file_client.cs
@@ -0,0 +1,105 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text; 
+
+namespace tcp
+{
+	class file_client
+	{
+		/// <summary>
+		/// The PORT.
+		/// </summary>
+		const int PORT = 9000;
+		/// <summary>
+		/// The BUFSIZE.
+		/// </summary>
+		const int BUFSIZE = 1000;
+        
+		public static string Desiredfile;
+
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="file_client"/> class.
+		/// </summary>
+		/// <param name='args'>
+		/// The command-line arguments. First ip-adress of the server. Second the filename
+		/// </param>
+		private file_client(string[] args)
+		{
+			TcpClient clientSocket = new TcpClient();
+			clientSocket.Connect(args[0], PORT);
+			Console.WriteLine("Client connected to server");
+
+			Desiredfile = args[1];
+
+  			NetworkStream serverStream = clientSocket.GetStream();
+
+			LIB.writeTextTCP(serverStream, Desiredfile);
+
+            //byte[] outStream = System.Text.Encoding.ASCII.GetBytes(Desiredfile + "$");
+            //serverStream.Write(outStream, 0, outStream.Length);
+            //serverStream.Flush();
+
+			receiveFile(Desiredfile, serverStream);            
+		}
+
+		/// <summary>
+		/// Receives the file.
+		/// </summary>
+		/// <param name='fileName'>
+		/// File name.
+		/// </param>
+		/// <param name='io'>
+		/// Network stream for reading from the server
+		/// </param>
+		private void receiveFile (String fileName, NetworkStream io)
+		{
+			long filesize = LIB.getFileSizeTCP(io);
+			Byte[] bytesReceived;
+			string ReceiveData = ""; 
+			int bytesRead = 0; 
+
+            if(filesize == 0)
+			{
+				Console.WriteLine("File did not exist, try again");
+				return; 
+			}
+           
+			Console.WriteLine($"File {LIB.extractFileName(fileName)} has been recieved");
+			Console.WriteLine($"Filesize: {filesize.ToString()}");
+			bytesReceived = new byte[BUFSIZE];
+
+            while ((bytesRead = io.Read(bytesReceived, 0, BUFSIZE)) > 0)
+			{
+				ReceiveData += Encoding.ASCII.GetString(bytesReceived);
+			}      
+            //SaveFile(ReceiveData);
+			Console.WriteLine($"Data received: {ReceiveData}");
+          
+        }
+
+        private void SaveFile(String filedata)
+		{
+			string path = "/home/ikn/Desktop/TextReceived.txt";
+			TextWriter tw = new StreamWriter(path,true);
+			tw.Write($"{filedata}");           
+			tw.Close();
+		}
+
+        
+		/// <summary>
+		/// The entry point of the program, where the program control starts and ends.
+		/// </summary>
+		/// <param name='args'>
+		/// The command-line arguments.
+		/// </param>
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Client starts...");
+			new file_client(args);
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Exercise_6_c# (SERVER)/Exercise_6_c#/file_server/file_server.cs
@@ -0,0 +1,132 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace tcp
+{
+	class file_server
+	{
+		/// <summary>
+		/// The PORT
+		/// </summary>
+		const int PORT = 9000;
+		/// <summary>
+		/// The BUFSIZE
+		/// </summary>
+		const int BUFSIZE = 1000;
+        
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="file_server"/> class.
+		/// Opretter en socket.
+		/// Venter på en connect fra en klient.
+		/// Modtager filnavn
+		/// Finder filstørrelsen
+		/// Kalder metoden sendFile
+		/// Lukker socketen og programmet
+ 		/// </summary>
+		private file_server ()
+		{
+			TcpListener serverSocket = new TcpListener(PORT);
+            TcpClient clientSocket = default(TcpClient);
+			RequestCount = 0; 
+			serverSocket.Start();
+			while (true)
+            {
+			//Wait till client & tcp connection has been established
+			Console.WriteLine("Waiting for client requests"); 
+			clientSocket = serverSocket.AcceptTcpClient();
+			Console.WriteLine("Connection from client has been accepted");
+            
+			//Infinite loop for monitoring requests from Clients
+            
+				//Reads data from NetworkSteam (data from Client) 
+                //Add client to request count
+			    RequestCount = RequestCount + 1;
+                //Establish network stream connection
+                NetworkStream networkStream = clientSocket.GetStream();
+               
+				//Get filename from client
+				string dataFromClient = LIB.readTextTCP(networkStream);
+				//Size of file 
+				Byte[] bytesReceived = Encoding.ASCII.GetBytes(dataFromClient);
+
+				Console.WriteLine($"File requested from client: {dataFromClient}");
+                
+				sendFile(dataFromClient, BitConverter.ToInt32(bytesReceived, 0), networkStream); 
+
+            }
+			clientSocket.Close();
+			serverSocket.Stop();
+			Console.WriteLine("Server has been shut down");
+			Console.ReadLine(); 
+		}
+
+		public int RequestCount { get; set; }
+
+		/// <summary>
+		/// Sends the file.
+		/// </summary>
+		/// <param name='fileName'>
+		/// The filename.
+		/// </param>
+		/// <param name='fileSize'>
+		/// The filesize.
+		/// </param>
+		/// <param name='io'>
+		/// Network stream for writing to the client.
+		/// </param>
+		private void sendFile (String fileName, long fileSize, NetworkStream io)
+		{
+			string serverResponse; 
+			Byte[] sendBytes; 
+
+			if(LIB.check_File_Exists(fileName) != 0)
+			{
+				Console.WriteLine($"Trying to send file: {fileName}");
+				FileStream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+
+
+				long sizeOfFile = LIB.check_File_Exists(fileName);
+				LIB.writeTextTCP(io, sizeOfFile.ToString());
+				io.Flush();
+
+				sendBytes = new byte[BUFSIZE];
+				int readBytes = 0; 
+				while((readBytes = fileStream.Read(sendBytes, 0, BUFSIZE)) > 0)
+				{
+					serverResponse = Encoding.ASCII.GetString(sendBytes);
+					LIB.writeTextTCP(io, serverResponse);
+				}
+                io.Flush();                 
+			}
+			else
+			{
+				Console.WriteLine($"The file {LIB.extractFileName(fileName)} did not exist");
+				//File was not found, send errormessage              
+				LIB.writeTextTCP(io, "0"); 
+				io.Flush();
+			}
+            //Flush data from stream 
+
+		}
+
+		/// <summary>
+		/// The entry point of the program, where the program control starts and ends.
+		/// </summary>
+		/// <param name='args'>
+		/// The command-line arguments.
+		/// </param>
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Server starts...");
+			var serverSocket = new file_server();          
+
+          }
+	}
+}
