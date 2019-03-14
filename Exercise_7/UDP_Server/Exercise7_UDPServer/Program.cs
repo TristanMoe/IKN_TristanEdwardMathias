8097e418230f5d8f6f134300b0966f2f0e0f7bd9
SuperWeebTanks <trilleo12@live.dk>
2019-03-14 14:12:17 +0100

    Added UDP exercise

---
+++ Exercise_7/UDP_Server/Exercise7_UDPServer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Exercise7_UDPServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+			var UDP_listener = new UDP_Client_Listener();
+			UDP_listener.StartUDPServer(); 
+            
+        }
+    }
+}
